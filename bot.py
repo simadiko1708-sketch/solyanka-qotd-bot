bf6d7f5919681ab90296550ff325ef51919a6250
simadiko1708-sketch <simadiko1708@gmail.com>
2026-08-04 00:36:07 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,27 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+@bot.event
+async def on_ready():
+    print(f"Бот запущен: {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong!")
+
+bot.run(TOKEN)
